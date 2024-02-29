a0185c4e095d4479b3850aea42023459cb8829f0
rpramu <pavan2762001@gmail.com>
2024-02-29 14:50:33 +0530

    Added  App.java

---
+++ simplemavenproject/src/main/java/com/cisco/simplemavenproject/App.java
@@ -0,0 +1,15 @@
+package com.cisco.simplemavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println( "Welcome to the world of the jenkins !" );
+        System.out.println("Simple project for jenkins");
+   
+    }
+}
